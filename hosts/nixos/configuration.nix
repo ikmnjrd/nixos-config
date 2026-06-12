23d8336d1ce1b9ac3b24749db1aa696a166445a7
ike <40803799+ikmnjrd@users.noreply.github.com>
2026-06-12 16:38:43 +0900

    fix: 日本語ホームディレクトリの掃除

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -5,6 +5,13 @@
 { config, lib, pkgs, ... }:
 
 let
+  gtkBookmarks = pkgs.writeText "gtk-bookmarks" ''
+    file:///home/ikd/Documents
+    file:///home/ikd/Music
+    file:///home/ikd/Pictures
+    file:///home/ikd/Videos
+    file:///home/ikd/Downloads
+  '';
   userDirs = pkgs.writeText "user-dirs.dirs" ''
     XDG_DESKTOP_DIR="$HOME/Desktop"
     XDG_DOWNLOAD_DIR="$HOME/Downloads"
@@ -147,7 +154,17 @@ in
     "d /home/ikd/Templates 0755 ikd users -"
     "d /home/ikd/Videos 0755 ikd users -"
     "d /home/ikd/.config 0755 ikd users -"
+    "d /home/ikd/.config/gtk-3.0 0755 ikd users -"
+    "L+ /home/ikd/.config/gtk-3.0/bookmarks - - - - ${gtkBookmarks}"
     "L+ /home/ikd/.config/user-dirs.dirs - - - - ${userDirs}"
+    "r /home/ikd/デスクトップ - - - - -"
+    "r /home/ikd/ダウンロード - - - - -"
+    "r /home/ikd/テンプレート - - - - -"
+    "r /home/ikd/公開 - - - - -"
+    "r /home/ikd/ドキュメント - - - - -"
+    "r /home/ikd/音楽 - - - - -"
+    "r /home/ikd/画像 - - - - -"
+    "r /home/ikd/ビデオ - - - - -"
   ];
 
   # Install firefox.
