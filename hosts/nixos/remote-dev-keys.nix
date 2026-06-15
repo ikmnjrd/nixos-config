1a723203da8fe1ddac03321038ad98321bb3c237
ike <40803799+ikmnjrd@users.noreply.github.com>
2026-06-16 01:21:18 +0900

    ike@mbpの公開鍵追加

--- hosts/nixos/remote-dev-keys.nix
+++ hosts/nixos/remote-dev-keys.nix
@@ -2,4 +2,5 @@
   # Add one public key per development host, for example:
   # "ssh-ed25519 AAAAC3... pc-remote-dev"
   # "ssh-ed25519 AAAAC3... mac-remote-dev"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+0Bucu1u0URL155NY+mZO7zTNCC6JEzhhDwP0DrO9G ike@ikembp.local"
 ]
